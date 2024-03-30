0472268e7f1796a47a1514319d6e6fcf3f930d36
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-30 22:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+ const video = document.getElementById('video');
+    const sound = document.getElementById('sound');
+    const timeDisplay = document.querySelector('.time-display');
+    let time = 10 * 60; // Initial time in seconds
+
+    // Function to change sound and video
+    function changeSound(type) {
+      video.src = `./Sounds and Videos/${type}.mp4`;
+      sound.src = `./Sounds and Videos/${type}.mp3`;
+    }
+
+    // Function to set time
+    function setTime(minutes) {
+      time = minutes * 60;
+      updateDisplay();
+    }
+
+    // Function to update time display
+    function updateDisplay() {
+      const minutes = Math.floor(time / 60);
+      const seconds = time % 60;
+      timeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+
+    // Function to toggle play/pause
+    function togglePlayPause() {
+      if (video.paused) {
+        video.play();
+        sound.play();
+        interval = setInterval(() => {
+          time--;
+          updateDisplay();
+          if (time <= 0) togglePlayPause();
+        }, 1000);
+      } else {
+        video.pause();
+        sound.pause();
+        clearInterval(interval);
+      }
+    }
